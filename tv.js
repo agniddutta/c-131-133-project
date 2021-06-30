60a6a33b25cd4cf908badf704995ef6121d1eb70
agniddutta <86725126+agniddutta@users.noreply.github.com>
2021-06-30 18:27:00 +0530

    Add files via upload

---
+++ tv.js
@@ -0,0 +1,45 @@
+img = "";
+status = "";
+objects = []
+
+function setup() {
+    canvas = createCanvas(640, 420);
+    canvas.center();
+    object_detector = ml5.objectDetector("cocossd", modelloaded);
+    document.getElementById("tv_status").innerHTML = "status: detecting objects";
+}
+
+function preload() {
+    img = loadImage("tv_and_ac.jpg")
+}
+
+function draw() {
+    image(img, 0, 0, 640, 420);
+    if (status != "") {
+        for (i = 0; i < objects.length; i++) {
+            document.getElementById("tv_status").innerHTML = "status: object detected";
+            fill("red");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x, objects[i].y);
+            noFill();
+            stroke("red");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
+
+}
+
+function modelloaded() {
+    console.log("modelloaded");
+    status = true;
+    object_detector.detect(img, gotresults);
+}
+
+function gotresults(error, results) {
+    if (error) {
+        console.log(error)
+    } else {
+        console.log(results);
+        objects = results
+    }
+}
